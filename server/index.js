53114dfd9ffe0348be6fef05097f91b4d3686900
gin280 <zxy_wtfm@163.com>
2023-08-21 20:16:06 +0800

    change code

--- server/index.js
+++ server/index.js
@@ -9,7 +9,15 @@ import dalleRoutes from "./routes/dalleRoutes.js"
 dotenv.config()
 
 const app = express()
-app.use(cors())
+// app.use(cors())
+app.use(
+  cors({
+    origin: "https://main--melodic-moonbeam-46c7c6.netlify.app/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+)
 app.use(express.json({ limit: "50mb" }))
 
 app.use("/api/v1/post", postRoutes)
