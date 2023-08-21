cff54fb60cec509f8f4d637d47a230f3a8707fba
gin280 <zxy_wtfm@163.com>
2023-08-21 21:30:34 +0800

    change code

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -25,7 +25,7 @@ router.route("/").post(async (req, res) => {
       response_format: "b64_json",
     })
     console.log(aiResponse, "aiResponse")
-    const image = aiResponse.data.data[0].b64_json
+    const image = aiResponse?.data?.[0].b64_json
     res.status(200).json({ photo: image })
   } catch (error) {
     console.error(error)
